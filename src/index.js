eae7f39baa0563c6594f833cddda1b2958720acb
emileCda <emile.cda@gmail.com>
2023-02-07 17:37:55 +0100

    correction des remarques + ajout de l'input pour choisir le pas des incréments et décrément s

--- src/index.js
+++ src/index.js
@@ -1,92 +1,95 @@
+const minValue = 0
+const resetValue = 0 // could be different that minValue
+let counter = resetValue // start with reset value
+let counterIncrement = 1 // default increment = 1
 
-const minValue = 0 ; 
-const resetValue = 0 ;        // could be different that minValue
-let counter = resetValue;     // start with reset value
-
-
+//------------------------------------------------------------------------
 /**
  * this fonction display the counter in the right place.
  * param  value to be display
- * return : void 
+ * return : void
  */
-
-function displayCounter(value){
-  
-  const myDisplay=document.querySelector('.counter-display');
-  myDisplay.innerText = value;
-
+function displayCounter(value) {
+  const myDisplay = document.querySelector('.counter-display')
+  myDisplay.innerText = value
 }
 
 //------------------------------------------------------------------------
 /**
- * this function increment the counter and display it 
+ * this function increment the counter and display it
  * param : void
- * return : void 
- *  */
-function incrementCounter(){
-
-  counter +=1;
-  displayCounter(counter);
-
+ * return : void
+ */
+function incrementCounter() {
+  counter += counterIncrement
+  displayCounter(counter)
 }
 
 //------------------------------------------------------------------------
 /**
- * this function decrement the counter display it 
+ * this function decrement the counter display it
  * warning : counter should never be lower than min-value
  * param : void
- * return : void 
- *  */
-function decrementCounter(){
-
-  console.log(" incrementCounter");
-  if (counter > minValue){
-    counter -=1;
-  }
-  else{
-    counter = minValue;
+ * return : void
+ */
+function decrementCounter() {
+  if (counter - counterIncrement >= minValue) {
+    counter -= counterIncrement
+  } else {
+    counter = minValue
   }
 
-  displayCounter(counter);
-
+  displayCounter(counter)
 }
-
 //------------------------------------------------------------------------
 /**
  * this function set the  counter to the resetValue
  * param : void
- * return : void 
- *  
- * */
-function resetCounter(){
-
-  counter = resetValue; 
-  displayCounter(counter);
-
+ * return : void
+ *
+ */
+function resetCounter() {
+  counter = resetValue
+  displayCounter(counter)
 }
-
 //------------------------------------------------------------------------
 /**
- * this function initialize home page in order to keep an oppening light and fast 
+ * this function set the increment for counter step
+ * value come from input text and need to be cas in int (base10)
+ * if not int value then display current counterIncrement in the input
  * param : void
- * return : void 
+ * return : void
+ *
  */
+function changeIncrement() {
+  const myNumber = document.querySelector('.input-increment').value
 
-function initPage(){
-
+  counterIncrement = parseInt(myNumber, 10)
+}
+//------------------------------------------------------------------------
+/**
+ * this function initialize home page in order to keep an oppening light and fast
+ * param : void
+ * return : void
+ */
+function initPage() {
   // display counter  on init counter = reset value
-displayCounter(counter) ;
+  displayCounter(counter)
 
-// listener for each button (increment, decrement and reset )
-document.querySelector('.increment')
-        .addEventListener('click', incrementCounter)
-document.querySelector('.decrement')
-        .addEventListener('click', decrementCounter)
-document.querySelector('.reset')
-        .addEventListener('click', resetCounter)
+  // listener for each button (increment, decrement and reset )
+  document
+    .querySelector('.increment')
+    .addEventListener('click', incrementCounter)
+  document
+    .querySelector('.decrement')
+    .addEventListener('click', decrementCounter)
+  document.querySelector('.reset').addEventListener('click', resetCounter)
 
+  document
+    .querySelector('.input-increment')
+    .addEventListener('change', changeIncrement)
+
+  document.querySelector('.input-increment').value = counterIncrement
 }
 
-
-
-document.addEventListener("DOMContentLoaded",initPage); 	// waiting for document loaded before code executing
+document.addEventListener('DOMContentLoaded', initPage) // waiting for document loaded before code executing
