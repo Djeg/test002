77af08dbd53e84d485085fcb2eeb789b5536710c
RAFFOUX Nicolas <nicolas1.raffoux@gmail.com>
2023-02-07 15:42:15 +0100

    Recorrection

--- src/increment.js
+++ src/increment.js
@@ -6,21 +6,18 @@ export function operation(increment = false, decremente = false, reset = false){
     const display = document.querySelector('.counter')
 
     if(reset){
-        console.log('reset')
         compteur = 0
         display.innerText = compteur
         return
     }
 
     if(increment){
-        console.log('add')
         compteur += 1
         display.innerText = compteur
         return
     }
 
     if(decremente){
-        console.log('false')
         compteur -= 1
         if(compteur <= 0){
             compteur = 0
