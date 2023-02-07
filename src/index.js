91fb7fcadd5b1b2a7c95280ef934d10b5075af95
kaoutar <kaoutar1301@gmail.com>
2023-02-07 15:45:54 +0100

    premier commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("Bonjour :)");
+
+
+const plus = document.querySelector(".plus")
+const moin = document.querySelector(".moin")
+const reset = document.querySelector(".reset")
+const counter= document.querySelector(".counter")
+let cpt=0
+counter.textContent=cpt
+
+plus.addEventListener('click',function(){
+       cpt++ 
+       counter.textContent=cpt
+    })
+
+moin.addEventListener('click',function(){
+    if(cpt>0){
+        cpt--
+        counter.textContent=cpt
+    }
+})
+reset.addEventListener('click',function(){
+       cpt=0
+       counter.textContent=cpt
+})
+
+
