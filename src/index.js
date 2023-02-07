bd672e62838b476a38dec5408c23ca3eb554888a
Kaoutar BAHLOULI <bahloulikaoutar@gmail.com>
2023-02-07 15:11:19 +0100

    compteur version2

--- src/index.js
+++ src/index.js
@@ -1,39 +1,33 @@
-console.log('Bonjour :)')
-
 //Recuper la valeur de span
 const valeurCompteur = document.querySelector('.counter')
 
 //créer un événement au buton + plus
-document.querySelector('.plus').addEventListener('click', CompteurPlus)
+document.querySelector('.plus').addEventListener('click', compteurPlus)
 
 let compteur = 0
 //Ajouter la fonction compteur plus
-function CompteurPlus() {
+function compteurPlus() {
   compteur += 1
-  console.log(compteur)
   valeurCompteur.innerHTML = compteur
 }
 
 //créer un événement au buton + minus
-document.querySelector('.minus').addEventListener('click', CompteurMinus)
-// créer la fonction CompteurMinus
-function CompteurMinus() {
+document.querySelector('.minus').addEventListener('click', compteurMinus)
+// créer la fonction compteurMinus
+function compteurMinus() {
   if (compteur <= 0) {
     compteur = 0
-    console.log(compteur)
     valeurCompteur.innerHTML = compteur
   } else {
     compteur -= 1
-    console.log(compteur)
     valeurCompteur.innerHTML = compteur
   }
 }
 
 //créer un événement au buton RESET
-document.querySelector('.reset').addEventListener('click', CompteurReset)
+document.querySelector('.reset').addEventListener('click', compteurReset)
 // créer la fonction reset
-function CompteurReset() {
+function compteurReset() {
   compteur = 0
-  console.log(compteur)
   valeurCompteur.innerHTML = compteur
 }
