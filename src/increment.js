7049786acfc0d386f85384f335f563928eff9908
RAFFOUX Nicolas <nicolas1.raffoux@gmail.com>
2023-02-07 15:36:46 +0100

    Correction

--- src/increment.js
+++ src/increment.js
@@ -2,30 +2,30 @@
 let compteur = 0
 
 //Incrémente et décrémente le compteur en fonction du bouton appuyé
-export function operation(op, reset = false){
+export function operation(increment = false, decremente = false, reset = false){
     const display = document.querySelector('.counter')
 
     if(reset){
+        console.log('reset')
         compteur = 0
         display.innerText = compteur
         return
     }
 
-    if(op === true){
+    if(increment){
+        console.log('add')
         compteur += 1
         display.innerText = compteur
         return
     }
 
-    if(op === false){
+    if(decremente){
+        console.log('false')
         compteur -= 1
         if(compteur <= 0){
             compteur = 0
-            display.innerText = compteur
         }
-        else{
-            display.innerText = compteur
-            return
-        }
+        display.innerText = compteur
+        return
     }
 }
--- src/index.js
+++ src/index.js
@@ -4,24 +4,18 @@ const btnAdd = document.querySelector('.plus')
 const btnSub = document.querySelector('.minus')
 const btnReset = document.querySelector('.reset')
 
-//Initialisation des variables
-let increment = false
-let reset = false
 
 //Incrémenter le compteur quand le button + est appuyé
-btnAdd.addEventListener('click', () => {
-    increment = true
-    operation(increment)
+btnAdd.addEventListener('click', () =>{
+    operation(true, false, false)
 })
 
 //Décrementer le compteur quand le button - est appuyé
-btnSub.addEventListener('click', () => {
-    increment = false
-    operation(increment)
+btnSub.addEventListener('click', () =>{
+    operation(false, true, false)
 })
 
 //Reset a 0 le compteur quand le button reset est appuyé
-btnReset.addEventListener('click', () => {
-    reset = true
-    operation(increment, reset)
+btnReset.addEventListener('click', () =>{
+    operation(false, false, true)
 })
