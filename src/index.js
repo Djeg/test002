4b27e347225149e710397910ea4072b98533f896
Reza <mrneyestani@gmail.com>
2023-04-16 16:26:58 +0200

    add bg and style

--- src/index.js
+++ src/index.js
@@ -1,38 +1,11 @@
-/*
-Exercice du compteur
-*/
+import { initializeCounter } from './counter'
 
-/*
-En utilisant des fonctions :
-- Lorsque je clique sur "+" le compteur augmente de 1
-- Lorsque je clique sur "-" le compteur diminue de 1
-- Lorsque je clique sur "reset" le compteur se remet à 0
-*/
-
-const buttonPlus = document.querySelector('.plus')
-const buttonMinus = document.querySelector('.minus')
-const buttonReset = document.querySelector('.reset')
-let compteur = document.querySelector('.counter')
-const input = document.querySelector('input.nombre')
-let x = 0
-input.value = 0
-
-buttonPlus.addEventListener('click', () => {
-  x = x + parseInt(input.value)
-  console.log((compteur.innerText = x))
-})
-
-buttonMinus.addEventListener('click', () => {
-  if (x > 0) {
-    x = x - parseInt(input.value)
-  } else {
-    x = 0
-  }
-  console.log((compteur.innerText = x))
-})
-
-buttonReset.addEventListener('click', () => {
-  x = 0
-  input.value = 0
-  console.log((compteur.innerText = x))
+initializeCounter({
+  counterPlus: document.querySelector('.counter-container .button.plus'),
+  counterMinus: document.querySelector('.counter-container .button.minus'),
+  incrementPlus: document.querySelector('.increment-container .button.plus'),
+  incrementMinus: document.querySelector('.increment-container .button.minus'),
+  reset: document.querySelector('.button.reset'),
+  counter: document.querySelector('.amount.counter'),
+  increment: document.querySelector('.amount.increment'),
 })
